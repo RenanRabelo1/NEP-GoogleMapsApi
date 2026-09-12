217f54891f12ce3d29ec1119a97b6f08ccda2016
Renan <renanrabelo82@gmail.com>
2026-09-12 11:31:09 -0300

    feat(mapa): adiciona marcadores pelo formulário

--- src/features/mapa/mapa.js
+++ src/features/mapa/mapa.js
@@ -1,14 +1,34 @@
+let mapa;
+
 function inicializarMapa() {
   const opcoesMapa = {
     center: { lat: -3.7683, lng: -38.4797 },
     zoom: 16
   };
 
-  return new google.maps.Map(
+  mapa = new google.maps.Map(
     document.getElementById("map"),
     opcoesMapa
   );
+
+  return mapa;
 }
 
 function adicionarMarcador(latitude, longitude, titulo) {
+  return new google.maps.Marker({
+    map: mapa,
+    position: { lat: latitude, lng: longitude },
+    title: titulo,
+    draggable: true
+  });
 }
+
+const botaoAdicionarPonto = document.getElementById("adicionar-ponto");
+const campoCoordenadas = document.getElementById("coordenadas");
+const campoTitulo = document.getElementById("titulo-marcador");
+
+botaoAdicionarPonto.addEventListener("click", () => {
+  const [latitude, longitude] = campoCoordenadas.value.split(",").map(Number);
+
+  adicionarMarcador(latitude, longitude, campoTitulo.value);
+});
